fac53b79df91a9ff7f610faad793b61f33bde523
wsmelo <wagner1005.55@gmail.com>
2026-08-25 19:05:09 -0300

    Update supabaseAnonKey in config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ export const RAFFLE_CONFIG = Object.freeze({
   maxSelection: 20,
   preSoldNumbers: [7, 13, 18, 27, 43, 49],
     supabaseUrl: "https://kcptfvgvpoghgdkcoyka.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjcHRmdmd2cG9naGdka2NveWthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2MDQxNjcsImV4cCI6MjEwMzE4MDE2N30.-iF4Byi9YdmWGGDoaCC0zEqy8FJLXNqMX-9PaBzyTTk",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjcHRmdmd2cG9naGdka2NveWthIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NzYwNDE2NywiZXhwIjoyMTAzMTgwMTY3fQ.rnz5qkeAYxcN3lycF9wFy9aCRqVZiU_jeRrNSQZeCUQ",
 });
 
 export function hasConfiguredPixKey() {
